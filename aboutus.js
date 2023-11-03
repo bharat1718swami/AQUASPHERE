44d6b3fa6c5003aff1fde2084c1039d75356f9e1
Yadav-Vishnu <yadav143vishnu@gmail.com>
2023-11-04 05:13:14 +0530

    Initial commit

---
+++ aboutus.js
@@ -0,0 +1,14 @@
+let p = document.getElementById('about');
+console.log(p.innerText);
+
+let text = p.innnerText;
+console.log(text.length);
+// p.innerText ="";
+// function print(text , p.innerText){
+//     for(let i=0;i<text.length,i=i+1){
+//         p.innerText += text[i];
+//     }
+//     return ;
+// }
+
+// print(text,p.innerText)
